f899b3fb5863e0ea1bed7fc38fb7519c76ba94c2
fabiobatoni <fabio_batoni@hotmail.com>
2024-07-26 14:11:29 -0300

    Interface - Separating components

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return(
+    <div className="flex flex-col gap-1">
+      {/* Header */}
+      <h1 className="text-2xl font-bold">Fundamentos do Redux</h1>
+      <span className="text-sm text-zinc-400">Modulo "Desvendando o Redux"</span>
+    </div>
+  )
+}
